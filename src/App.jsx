dc43f8f6bf45ed4db648687a3991b95b68e5792d
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-04-22 12:59:32 -0300

    correccion bug de overflow

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import AboutMe from '@sections/AboutMe';
 
 export default function App() {
   return (
-    <div className="w-screen min-h-screen flex justify-center bg-slate-900">
+    <div className="w-full min-h-screen flex justify-center bg-slate-900 overflow-x-hidden">
       <div className="lg:max-w-[1024px] lg:min-w-[1024px] w-full h-full relative">
         <Navbar/>
         <main className="flex flex-col gap-2">
