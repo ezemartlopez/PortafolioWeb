68f1bc45e9329a657710e3818022cb6671a907ea
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-04-22 12:59:56 -0300

    creando la base de los proyectos archivo de consulta.

---
+++ src/data/projects.js
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+        "titulo":"SVGL - A beautiful library with SVG logos",
+        "tecnologias":[],
+        "descripcion":"Biblioteca de logos SVG de las marcas más populares. +10k visitas al mes. +2K svgs descargados. Creado desde cero con Next.js, React y Tailwind CSS.",
+        "url_github":"link",
+        "url_preview":"link",
+        "url_image":"https://fastly.picsum.photos/id/1013/536/354.jpg?hmac=g-noHNvrLjQNz62ByvWlOk-g2K4_LNhFKl8THdi9FU8"
+    }
+];
