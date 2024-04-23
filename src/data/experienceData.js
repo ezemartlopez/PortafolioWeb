033319d08876587783639d4792bd38231da3b561
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-04-23 16:49:00 -0300

    modifique la data de experiencia.

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,13 +3,12 @@ export const experience = [
         "cargo":"Desarrollador Frontend",
         "lugar":"Freelance",
         "tiempo":"Actualidad",
-        "detalles":"Como desarrollador frontend freelance, he llevado a cabo una serie de proyectos personales con el objetivo de fortalecer y ampliar mis habilidades en el desarrollo web."
+        "detalles":"Desarrollando proyectos personales destinados a potenciar y ampliar mis habilidades como desarrollador frontend. Estos proyectos son mi campo de pruebas para explorar y dominar nuevas tecnologías de vanguardia, demostrando mi compromiso continuo con el aprendizaje y la innovación en el mundo del desarrollo web."
     },
     {
         "cargo": "Desarrollador Frontend",
         "lugar":"UTN Buenos Aires (Proyecto Aceleradora)",
         "tiempo": "Septiembre - Diciembre (2023)",
-        "detalles": 
-            "Como desarrollador frontend en el proyecto de Copal, me dediqué a crear interfaces de usuario atractivas y funcionales utilizando tecnologías como React y Angular."
+        "detalles": "Durante mi pasantía universitaria, lideré un proyecto ágil para desarrollar una aplicación web personalizada para la Coordinadora de Industrias Alimentarias COPAL. Me encargué de definir la estrategia de desarrollo y maquetado de la aplicación. Trabajé en estrecha colaboración con un equipo dinámico para garantizar una estructura sólida y modular que permitiera una rápida adaptación a las necesidades del cliente."
     }
 ];
