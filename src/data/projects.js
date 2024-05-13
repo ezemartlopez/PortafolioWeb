a0a4ca0e8d1bbc09a753258043ef75ebc044d773
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-05-12 22:13:47 -0300

    proyecto film-flix agregado

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,5 +6,13 @@ export const projects = [
         "url_github":"https://github.com/ezemartlopez/todolist",
         "url_preview":"https://todolistmle.netlify.app/",
         "url_image":"https://raw.githubusercontent.com/ezemartlopez/ImgenesPortafolio/main/todolistview.png"
-    }
+    },
+    {
+        "titulo":"Film-Flix",
+        "tecnologias":["REACT", "TAILWIND"],
+        "descripcion":"Film-Flix es un buscador de películas y series donde los usuarios pueden explorar y descubrir una amplia variedad de contenido. Buscando por el título se puede obtener información detallada (obtenida de la API de OMDB), incluyendo sinopsis, elenco y calificaciones.",
+        "url_github":"https://github.com/ezemartlopez/filmflix",
+        "url_preview":"https://filmflixmle.netlify.app/",
+        "url_image":"https://raw.githubusercontent.com/ezemartlopez/ImgenesPortafolio/main/filmflix.png"
+    },
 ];
